09ed1882289d6375d2d0d3a585c4dec6f262c34b
JaeinChoii <hightreehuman@gmail.com>
2022-11-02 14:04:48 +0900

    study: 파일업로드 single

---
+++ 13_file_upload/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+// multer 설정
+const multer = require('multer');
+const path = require('path');
+const upload = multer({
+    dest: 'uploads/',
+});
+const uploadDetail = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            // req: 요청에 대한 정보
+            // file: 업로드하는 파일에 대한 정보
+            // done(에러, 저장경로): 함수(콜백함수)
+            done(null, 'uploads/'); // 경로설정
+        },
+        filename(req, file, done) {
+            // req: 요청에 대한 정보
+            // file: 업로드하는 파일에 대한 정보
+            // done: 함수
+            const ext = path.extname(file.originalname); // file.originalname에서 "확장자" 추출
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+            // [파일명+현재시간.확장자] 이름으로 바꿔서 파일 업로드
+            // 현재시간: 파일명이 겹치는 것을 막기 위함
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.render('index', { title: '파일 업로드를 배워보자!' });
+});
+
+// 1. single(): 하나의 파일 업로드할 때
+// single()의 인자: input 태그의 name 값
+// single() -> req.file 객체에 파일 정보
+// app.post('/upload', upload.single('userfile'), function (req, res) {
+app.post('/upload', uploadDetail.single('userfile'), function (req, res) {
+
+    // req.file: 파일 업로드 성공 결과 (파일 정보)
+    
+    //   {
+    //     fieldname: 'userfile', // 폼에 정의된 name
+    //     originalname: '1.jpg', // 원본 파일명
+    //     encoding: '7bit', // 파일 엔코딩 설정
+    //     mimetype: 'image/jpeg', // 파일 타입
+    //     destination: '/uploads', // 파일을 저장할 경로(폴더)
+    //     filename: '37bcd7d03ff1a736ab4be77a4edb4b84', // destination에 저장된 파일명
+    //     path: '\\uploads\\37bcd7d03ff1a736ab4be77a4edb4b84', // 업로드된 파일 전체 경로
+    //     size: 2554179 // 파일 크기 (byte)
+    //   }
+    console.log(req.file);
+    // req.body: title 데이터 정보 확인 가능
+    // [Object: null prototype] { title: '1' }
+
+    console.log(req.body);
+  
+    res.send('Uploads!');
+  });
+
+app.listen(PORT, function (req, res) {
+  console.log(`http://localhost:${PORT}`);
+});
